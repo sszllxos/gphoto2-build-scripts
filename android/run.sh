9fa5bacf1a9a521d01431da7da2803c0a37f69f7
sszllx <lij80@163.com>
2024-11-01 21:23:02 +0800

    update

--- android/run.sh
+++ android/run.sh
@@ -8,6 +8,8 @@ if [ "$1" == "build" ]; then
 elif [ "$1" == "run" ]; then
     echo "Running Docker container..."
     docker run -v /data/code/workspace:/workspace -v $(pwd):/scripts android_build
+elif [ "$1" == "shell" ]; then
+    docker run -v /data/code/workspace:/workspace -v $(pwd):/scripts -it android_build /bin/bash
 else
     echo "Invalid argument. Use 'build' to build the image or 'run' to run the container."
     exit 1
