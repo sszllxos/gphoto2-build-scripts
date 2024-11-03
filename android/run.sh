ab3b76642667e42d4f852c23a0e56b1d16d749ba
lijng.ly <lijing.ly@bytedance.com>
2024-11-03 20:51:04 +0800

    camera detected

--- android/run.sh
+++ android/run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-
-
 if [ "$1" == "build" ]; then
     echo "Building Docker image..."
     docker build -t android_build .
 elif [ "$1" == "run" ]; then
     echo "Running Docker container..."
-    docker run -v /data/code/workspace:/workspace -v $(pwd):/scripts android_build
+    docker run -v /exdata3/lijing/code/workspace:/workspace -v $(pwd):/scripts -v /exdata3/lijing/code/data:/data android_build
 elif [ "$1" == "shell" ]; then
-    docker run -v /data/code/workspace:/workspace -v $(pwd):/scripts -it android_build /bin/bash
+    docker run -v /exdata3/lijing/code/workspace:/workspace -v $(pwd):/scripts -it android_build /bin/bash
 else
     echo "Invalid argument. Use 'build' to build the image or 'run' to run the container."
     exit 1
