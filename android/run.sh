49c95f7ebec1741ab28e7cd85148e609862163d4
sszllx <lij80@163.com>
2024-11-05 18:20:24 +0800

    update

--- android/run.sh
+++ android/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# CODEPATH="/exdata3/lijing/code"
+CODEPATH="/data/code"
+
 if [ "$1" == "build" ]; then
     echo "Building Docker image..."
     docker build -t android_build .
 elif [ "$1" == "run" ]; then
     echo "Running Docker container..."
-    docker run -v /exdata3/lijing/code/workspace:/workspace -v $(pwd):/scripts -v /exdata3/lijing/code/data:/data android_build
+    docker run -v ${CODEPATH}/workspace:/workspace -v $(pwd):/scripts -v ${CODEPATH}/data:/data android_build
 elif [ "$1" == "shell" ]; then
-    docker run -v /exdata3/lijing/code/workspace:/workspace -v $(pwd):/scripts -it android_build /bin/bash
+    docker run -v ${CODEPATH}/workspace:/workspace -v $(pwd):/scripts -it android_build /bin/bash
 else
     echo "Invalid argument. Use 'build' to build the image or 'run' to run the container."
     exit 1
